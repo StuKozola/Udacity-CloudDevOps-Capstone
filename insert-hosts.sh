8c71951a69fec21bb50393dd241b17d83615ff49
Stuart Kozola <stuart.kozola@gmail.com>
2021-04-01 15:05:29 -0400

    edits to hosts file insert

--- insert-hosts.sh
+++ insert-hosts.sh
@@ -1,48 +1,28 @@
 #!/usr/bin/env bash
 
-# sourced from https://gist.github.com/irazasyed/a7b0a079e7727a4315b9
-# usage: manage-etc-hosts [add|remove] <hostname> "<domain_names>"
+# https://stackoverflow.com/questions/19339248/append-line-to-etc-hosts-file-with-shell-script/19339320
 
-# Path to your hosts file
-hostsFile="hosts" #"/etc/hosts"
+# path to hosts file\
+hostfile="hosts" #"/etc/hosts"
 
-# Default IP address for host
-ip="0.0.0.0"
+# insert/update hosts entry
+ip_address="192.168.x.x"
+host_name="my.hostname.example.com"
+# find existing instances in the host file and save the line numbers
+matches_in_hosts="$(grep -n $host_name /etc/hosts | cut -f1 -d:)"
+host_entry="${ip_address} ${host_name}"
 
-# Hostname to add/remove.
-hostname="$2"
+echo "Please enter your password if requested."
 
-yell() { echo "$0: $*" >&2; }
-die() { yell "$*"; exit 111; }
-try() { "$@" || die "cannot $*"; }
-
-if [ -z "$1" ] || [ -z "$2" ]; then
-   die "Exiting... invalid arguments";
+if [ ! -z "$matches_in_hosts" ]
+then
+    echo "Updating existing hosts entry."
+    # iterate over the line numbers on which matches were found
+    while read -r line_number; do
+        # replace the text of each line with the desired host entry
+        sudo sed -i '' "${line_number}s/.*/${host_entry} /" /etc/hosts
+    done <<< "$matches_in_hosts"
+else
+    echo "Adding new hosts entry."
+    echo "$host_entry" | sudo tee -a /etc/hosts > /dev/null
 fi
-
-remove() {
-    if [ -n "$(grep -w "$hostname$" /etc/hosts)" ]; then
-        echo "$hostname found in $hostsFile. Removing now...";
-        try sudo sed -ie "/[[:space:]]$hostname/d" "$hostsFile";
-    else
-        yell "$hostname was not found in $hostsFile";
-    fi
-}
-
-add() {
-    if [ -n "$(grep -P "[[:space:]]$hostname" /etc/hosts)" ]; then
-        yell "$hostname, already exists: $(grep $hostname $hostsFile)";
-    else
-        echo "Adding $hostname to $hostsFile...";
-        try printf "%s\t%s\n" "$ip" "$hostname" | sudo tee -a "$hostsFile" > /dev/null;
-
-        if [ -n "$(grep $hostname /etc/hosts)" ]; then
-            echo "$hostname was added succesfully:";
-            echo "$(grep $hostname /etc/hosts)";
-        else
-            die "Failed to add $hostname";
-        fi
-    fi
-}
-
-$@
